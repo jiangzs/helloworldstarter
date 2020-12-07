e5a7da2a08284a1e125e8d1bcf239bf1db881f5b
john.jiang <john.jiang@wehotelglobal.com>
2020-12-07 13:41:56 +0800

    #init starter

---
+++ src/main/java/com/demo/HelloworldProperties.java
@@ -0,0 +1,22 @@
+package com.demo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author john.jiang@wehotelglobal.com jiangzs@gmail.com
+ **/
+@ConfigurationProperties(prefix = "helloworld")
+public class HelloworldProperties {
+
+    public static final String DEFAULT_WORDS = "world";
+
+    private String words = DEFAULT_WORDS;
+
+    public String getWords() {
+        return words;
+    }
+
+    public void setWords(String words) {
+        this.words = words;
+    }
+}
